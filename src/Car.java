9cfec789bb63a713ee873af8080eecd73b2c6497
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-01-18 09:44:55 +0100

    update

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+
+    private String displacement;
+    private String plate;
+    private String brand;
+
+    private String model;
+
+    public Car(String displacement, String plate, String brand, String model) {
+        this.displacement = displacement;
+        this.plate = plate;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(String displacement) {
+        this.displacement = displacement;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "displacement='" + displacement + '\'' +
+                ", plate='" + plate + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 }
